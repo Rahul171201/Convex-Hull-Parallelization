6f97e5ee55a8e54fb0a2326880c8503e0b1fff80
Rahul Roy <rahulroydps@gmail.com>
2022-10-30 14:46:56 +0530

    sorting algorithm added

--- graham_scan.cpp
+++ graham_scan.cpp
@@ -8,6 +8,36 @@ using namespace std;
 
 pair<int, int> bottom_most_point = {0, 0};
 
+// Function to know the orientation (clockwise or anticlockwise) of given 3 points
+// returns 0 if collinear, return 1 if clockwise, return -1 if anticlockwise
+int getCrossProduct(int a_x, int a_y, int b_x, int b_y, int c_x, int c_y)
+{
+    int val = ((b_y - a_y) * (c_x - b_x)) - ((b_x - a_x) * (c_y - a_y));
+    if (val > 0)
+        return 1;
+    else if (val < 0)
+        return -1;
+    else
+        return 0;
+}
+
+double distance(pair<int,int> p1, pair<int,int> p2){
+    double dis = sqrt((double(p2.second-p1.second)*double(p2.second-p1.second)) + (double(p2.first-p1.first)*double(p2.first-p1.first)));
+    return dis;
+}
+
+vector<int> grahamsScan(vector<pair<int,int>> points){
+    stack<pair<int,int>> s;
+    vector<pair<int,int>> convex_hull;
+    s.push(points[0]); convex_hull.push_back(points[0]);
+    s.push(points[1]); convex_hull.push_back(points[1]);
+    s.push(points[2]);
+
+    for(int i=3;i<points.size();i++){
+        
+    }
+}
+
 pair<int, int> getStartingPoint(int *x, int *y, int n)
 {
     pair<int, int> point = {-1, -1};
@@ -26,6 +56,13 @@ pair<int, int> getStartingPoint(int *x, int *y, int n)
     }
     point = {min_x, min_y};
     return point;
+}
+
+bool compare(const pair<int, int> &a, const pair<int, int> &b)
+{
+    double angle1 = (double(a.second - bottom_most_point.second) / double(a.first - bottom_most_point.first));
+    double angle2 = (double(b.second - bottom_most_point.second) / double(b.first - bottom_most_point.first));
+    return (angle1 < angle2);
 }
 
 // Main function
@@ -53,4 +90,49 @@ int main()
     pair<int, int> start_point = getStartingPoint(P.x, P.y, P.n);
 
     bottom_most_point = start_point;
+
+    vector<pair<int, int>> points;
+    points.push_back(start_point);
+
+    for (int i = 0; i < P.n; i++)
+    {
+        if ((P.x[i] < start_point.first) || (P.x[i] == start_point.first))
+            continue;
+        else
+            points.push_back({P.x[i], P.y[i]});
+    }
+
+    cout << "The bottom most point is = {" << start_point.first << ", " << start_point.second << "}\n";
+    sort(points.begin(), points.end(), compare);
+
+    for (int i = 0; i < P.n; i++)
+    {
+        if (P.x[i] == start_point.first && P.y[i] != start_point.second)
+            points.push_back({P.x[i], P.y[i]});
+    }
+
+    vector<pair<int, int>> temp_clockwise_points;
+    for (int i = 0; i < P.n; i++)
+    {
+        if ((P.x[i] > start_point.first) || (P.x[i] == start_point.first))
+            continue;
+        else
+            temp_clockwise_points.push_back({P.x[i], P.y[i]});
+    }
+    sort(temp_clockwise_points.begin(), temp_clockwise_points.end(), compare);
+
+    // pushing all elements on clockwise points vector to original points vector
+    for (int i = 0; i < temp_clockwise_points.size(); i++)
+        points.push_back(temp_clockwise_points[i]);
+
+    // printing final points vector
+    for (int i = 0; i < points.size(); i++)
+        cout << points[i].first << " " << points[i].second << "\n";
+
+    if(points.size() < 3){
+        cout<<"It is not possible to have a convex hull with the given number of points\n";
+        exit(0);
+    }
+
+    grahamsScan(points);
 }
