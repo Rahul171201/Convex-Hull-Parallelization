492f7f85e7fa359f589ed5dd4ce822f2ce4592cd
Rahul Roy <rahulroydps@gmail.com>
2022-10-30 13:23:30 +0530

    get starting point coded

--- graham_scan.cpp
+++ graham_scan.cpp
@@ -6,6 +6,28 @@ using namespace std;
 #define N 10       // number of points in the plane
 #define MAX_VAL 20 // maximum value of any point in the plane
 
+pair<int, int> bottom_most_point = {0, 0};
+
+pair<int, int> getStartingPoint(int *x, int *y, int n)
+{
+    pair<int, int> point = {-1, -1};
+    int min_y = INT_MAX, min_x = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        if (y[i] < min_y)
+        {
+            min_y = y[i];
+            min_x = x[i];
+        }
+        else if (y[i] == min_y)
+        {
+            min_x = min(min_x, x[i]);
+        }
+    }
+    point = {min_x, min_y};
+    return point;
+}
+
 // Main function
 int main()
 {
@@ -28,4 +50,7 @@ int main()
     // print the points in the plane
     P.printPoints(P.x, P.y, P.n);
 
+    pair<int, int> start_point = getStartingPoint(P.x, P.y, P.n);
+
+    bottom_most_point = start_point;
 }
